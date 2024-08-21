13dc8b01c047b60746a9e9db2f705fba4e3c32ce
Borja Martinez <bmjover@gmail.com>
2024-08-21 09:34:04 +0200

    Add more conditions to check availability of slot

--- calendar-spots/Calendar.ts
+++ calendar-spots/Calendar.ts
@@ -6,6 +6,9 @@ import { AvailableSlot, ScheduleCalendar, TimeSlot } from './types'
 
 class CalendarManager implements ICalendarManager {
   private calendar: ScheduleCalendar
+  private readonly __inputDateFormat = 'DD-MM-YYYY'
+  private readonly __outputDateFormat = 'YYYY-MM-DD'
+  private readonly __hourMinFormat = 'HH:mm'
 
   constructor(private calendarId: number) {
     this.calendar = this.loadCalendar()
@@ -13,15 +16,15 @@ class CalendarManager implements ICalendarManager {
 
   // Retrieves available slots from the calendar considering the date, session duration, and additional time before and after the session.
   public getAvailableSpots(date: string, durationInMin: number): AvailableSlot[] {
-    if (!moment(date, 'DD-MM-YYYY', true).isValid()) {
-      throw new Error('Invalid date format, expected DD-MM-YYYY')
+    if (!moment(date, this.__inputDateFormat, true).isValid()) {
+      throw new Error(`Invalid date format, expected ${this.__inputDateFormat}`)
     }
     if (durationInMin <= 0) {
       throw new Error('Duration must be greater than zero')
     }
 
     const { durationAfter, durationBefore, sessions, slots } = this.calendar
-    const dateISO = moment(date, 'DD-MM-YYYY').format('YYYY-MM-DD')
+    const dateISO = moment(date, this.__inputDateFormat).format(this.__outputDateFormat)
     const slotsOfDay = slots[date] ?? []
     const sessionsOfDay = sessions[date] ?? []
     const totalDuration = durationInMin + durationAfter + durationBefore
@@ -54,13 +57,26 @@ class CalendarManager implements ICalendarManager {
 
   // Check if the slot is available by checking for overlaps with current booked sessions
   private isSlotAvailable(sessions: TimeSlot[], slot: TimeSlot): boolean {
-    return !sessions.some((session) => session.start === slot.start && session.end === slot.end)
+    const slotStart = moment(slot.start, this.__hourMinFormat)
+    const slotEnd = moment(slot.end, this.__hourMinFormat)
+
+    return !sessions.some((session) => {
+      const sessionStart = moment(session.start, this.__hourMinFormat)
+      const sessionEnd = moment(session.end, this.__hourMinFormat)
+
+      return (
+        (slotStart.isSame(sessionStart) && slotEnd.isSame(sessionEnd)) || // This condition checks if the slot start and end times exactly match the session start and end times.
+        (slotStart.isAfter(sessionStart) && slotStart.isBefore(sessionEnd)) || // This condition checks if the slot start time falls within the session, but the end time may be outside the session.
+        (slotEnd.isAfter(sessionStart) && slotEnd.isBefore(sessionEnd)) || // This condition checks if the slot end time falls within the session, but the start time may be outside the session.
+        (slotStart.isBefore(sessionStart) && slotEnd.isAfter(sessionEnd)) // This condition checks if the slot completely overlaps the session, meaning it starts before and ends after the session.
+      )
+    })
   }
 
   // Checks if the duration of a given time slot is equal to or greater than a specified duration.
   private isSlotShiftEnough(slot: TimeSlot, requiredDuration: number): boolean {
-    const startTime = moment(slot.start, 'HH:mm')
-    const endTime = moment(slot.end, 'HH:mm')
+    const startTime = moment(slot.start, this.__hourMinFormat)
+    const endTime = moment(slot.end, this.__hourMinFormat)
     const slotDuration = endTime.diff(startTime, 'minutes')
 
     return slotDuration >= requiredDuration
